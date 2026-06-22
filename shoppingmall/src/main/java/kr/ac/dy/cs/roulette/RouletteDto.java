f96a5b1d09465bbe7c85a2f1d5fda7215cd56a78
qwerty141516 <jmunki06@dongyang.ac.kr>
2026-06-23 02:34:56 +0900

    [20251261-장문기] 룰렛 이벤트 결과 저장 DTO 클래스 추가

--- shoppingmall/src/main/java/kr/ac/dy/cs/roulette/RouletteDto.java
+++ shoppingmall/src/main/java/kr/ac/dy/cs/roulette/RouletteDto.java
@@ -1,1 +1,19 @@
 
+/*
+ 20251261 장문기
+ 쇼핑몰 룰렛 이벤트 결과 저장 DTO
+ */
+
+package kr.ac.dy.cs.roulette;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RouletteDto {
+    private String userId;
+    private int point;
+}
